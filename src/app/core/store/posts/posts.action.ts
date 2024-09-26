295bf844c7474700051ea38de7bfb173a67ccf47
burcineren <burcin.ern969@gmail.com>
2024-09-26 12:39:14 +0300

    feat: post state managment

--- src/app/core/store/posts/posts.action.ts
+++ src/app/core/store/posts/posts.action.ts
@@ -1,6 +1,4 @@
-import { Post } from '../../posts/posts.model';
 
 export class PostsAction {
-  static readonly type = '[Posts] Post';
-  constructor(public posts: Post) {}
+  static readonly type = '[Posts] Get Post';
 }
